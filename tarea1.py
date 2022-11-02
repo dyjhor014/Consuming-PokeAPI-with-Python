5bffffaaa96813bb951a4fbda44c1ff8d46eb0bd
dyjhor014 <jordycs93@gmail.com>
2022-11-02 14:24:56 -0500

    se creo la funcion para listar libros

--- tarea1.py
+++ tarea1.py
@@ -18,8 +18,11 @@ class Libro:
         self.editorial = editorial
         self.autor = autor
 
-    def agregar_libro(self):
-        return print("Agregar libro")
+    def cargar_libros():
+        return print(data.iloc[0:3])
+
+    def listar_libros():
+        return print(data)
 
     def eliminar_libro(self):
         answer_pos = data[data.index == id_eliminar]
@@ -33,31 +36,13 @@ class Libro:
         else:
             return print("Cancelaste la operacion")
 
-#FUNCIONES DEL PROGRAMA
-def leer_archivo():
-    return print(data.iloc[0:3])
-    
-def listar_libros():
-    return print(data)
-
-def buscar_libro_isbn_titulo():
-    return print("Buscar libro por ISBN o por título. Se debe sugerir las opciones y listar el resultado")
-
-def ordenar_libros():
-    return print("Ordenar libros por título.")
-
-def buscar_libro_autor_editorial_genero():
-    return print("Buscar libros por autor, editorial o género. Se deben sugerir las opciones y listar los resultados.")
-
-def buscar_libro_num_autores():
-    return print("Buscar libros por número de autores. Se debe ingresar un número por ejemplo 2 (hace referencia a dos autores) y se deben listar todos los libros que contengan 2 autores.")
-
-def actualizar_datos_libro():
-    return print("Editar o actualizar datos de un libro (título, género, ISBN, editorial y autores).")
-
-def guardar_libro():
-    return print("Guardar libros en archivo de disco duro (.txt o csv).")
-
-libro = Libro(2,"la ciudad y los perros","novela",987654321,"la imprenta","Mario Vargas Llosa")
-id_eliminar = int(input("Ingresa el id del libro que deseas eliminar\n"))
-Libro.eliminar_libro(id_eliminar)
+opcion_elegida = str(input("\nIngresa un número\n"))
+while opcion_elegida not in ("1","2","3","4","5","6","7","8","9","10"):
+    opcion_elegida = str(input("Debes ingresar uno de los números del menú del 1 al 10. Ingresa nuevamente tu respuesta\n"))
+if opcion_elegida == "1":
+    Libro.cargar_libros()
+if opcion_elegida == "2":
+    Libro.listar_libros()
+if opcion_elegida == "4":
+    id_eliminar = int(input("Ingresa el id del libro que deseas eliminar\n"))
+    Libro.eliminar_libro(id_eliminar)
