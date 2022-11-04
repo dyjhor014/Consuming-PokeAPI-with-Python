6c3eaf1b4f4be59c21a588f8ed3a4ee189297a92
dyjhor014 <jordycs93@gmail.com>
2022-11-04 15:48:42 -0500

    Se agregaron las funciones para la opcion 4 del menu

--- tarea2.py
+++ tarea2.py
@@ -1,29 +1,33 @@
 import os
+import requests
 os.system("clear")
 
-url_pokeapi = "https://pokeapi.co/api/v2/generation/"
+#Funcion para la opcion 4
+def listar_habitat(url: str) -> None:
 
-import requests
+        response_habitat = requests.get(url)
+        data_habitat = response_habitat.json()
+        for i in range(0, len(data_habitat["results"])):
+            habitat = data_habitat["results"][i]["name"]
+            print(f"Habitat ({i+1}): {habitat}")
+#Funcion para listar los pokemons segun el habitat elegido
+def listar_pokemon_habitat(seleccion_habitat: str) -> None:
+    while seleccion_habitat not in ("1","2","3","4","5","6","7","8","9"):
+        seleccion_habitat = str(input("Debes ingresar el numero del habitat\n"))
+    url = f'https://pokeapi.co/api/v2/pokemon-habitat/{seleccion_habitat}/'
+    response_habitat = requests.get(url)
+    data_habitat = response_habitat.json()
+    print(data_habitat["pokemon_species"][0]["name"])
+    print(f"Lista de pokemons con el habitat{seleccion_habitat}")
+    for i in range(0, len(data_habitat["pokemon_species"])):
+        pokemon_specie = data_habitat["pokemon_species"][i]["name"]
+        print(f"Pokemon ({i+1}): {pokemon_specie}")
 
-print("Bienvenido al programa para listar pokemons")
-print("*"*45)
-print("Opción 1:Listar pokemons por generación.")
-print("Opción 2:Listar pokemons por forma.")
-print("Opción 3:Listar pokemons por habilidad.")
-print("Opción 4:Listar pokemons por habitat")
-print("Opción 5:Listar pokemons por tipo.")
-
-opcion = int(input("\nIngrese el número de opción: "))
-if opcion == 1:
-     
-    def list_generation():
-        g = str(input("\nIngrese el número de generación: "))
-        #Haciendo el request (method: GET)
-        response = requests.get(url_pokeapi + g )
-        #Transformando respuesta de Json a Dict
-        data_g = response.json()
-        #Obteniendo pokemones de generacion ingresada:
-
-        print(f"\nLos pokemones de la generación {g} son en total: {len(data_g['pokemon_species'])}")
-           
-    list_generation()
+opcion = str(input("Ingresa una opción del Menu 1,2,3,4 o 5\n"))
+while opcion not in ("1","2","3","4","5"):
+    opcion = str(input("Debes ingresar una opción del 1 al 6\n"))
+if opcion == "4":
+    url = 'https://pokeapi.co/api/v2/pokemon-habitat/'
+    listar_habitat(url)
+    seleccion_habitat = str(input("Ingresa el numero del habitat\n"))
+    listar_pokemon_habitat(seleccion_habitat)
