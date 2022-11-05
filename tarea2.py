3b632f09257a0782de00af82b0f0d64943d28ac2
dyjhor014 <jordycs93@gmail.com>
2022-11-05 11:02:34 -0500

    Se agregó la funcionalidad para la opcion 1

--- tarea2.py
+++ tarea2.py
@@ -3,7 +3,7 @@ import requests
 os.system("clear")
 
 #Funcion para listar las opciones del menú
-def listar_habitat(seleccion_menu: str) -> None:
+def listar(seleccion_menu: str) -> None:
         url = f"https://pokeapi.co/api/v2/{seleccion_menu}/"
         response = requests.get(url)
         data = response.json()
@@ -12,7 +12,7 @@ def listar_habitat(seleccion_menu: str) -> None:
         print(f"{'*'*50}\n")
         for i in range(0, len(data["results"])):
             item = data["results"][i]["name"]
-            print(f"Habitat ({i+1}): {item}")
+            print(f"{seleccion_menu} ({i+1}): {item}")
         print("\n")
 
 #Funcion para listar los pokemons segun la opcion elegida
@@ -25,6 +25,10 @@ def listar_pokemon(seleccion, seleccion_menu, opcion: str) -> None:
     print(f"{'*'*50}\n")
     print(f"Lista de pokemons con el {seleccion_menu}: {seleccion}\n")
     print(f"{'*'*50}\n")
+    if opcion == "1":
+        for i in range(0, len(data["pokemon_species"])):
+            pokemon = data["pokemon_species"][i]["name"]
+            print(f"Pokemon ({i}): {pokemon}")
     if opcion == "4":
         for i in range(0, len(data["pokemon_species"])):
             pokemon = data["pokemon_species"][i]["name"]
@@ -38,13 +42,18 @@ def listar_pokemon(seleccion, seleccion_menu, opcion: str) -> None:
 opcion = str(input("Ingresa una opción del Menu 1,2,3,4 o 5\n"))
 while opcion not in ("1","2","3","4","5"):
     opcion = str(input("Debes ingresar una opción del 1 al 6\n"))
+if opcion == "1":
+    seleccion_menu = "generation"
+    listar(seleccion_menu)
+    seleccion = str(input(f"Ingresa el numero del {seleccion_menu}\n"))
+    listar_pokemon(seleccion, seleccion_menu, opcion)
 if opcion == "4":
     seleccion_menu = "pokemon-habitat"
-    listar_habitat(seleccion_menu)
+    listar(seleccion_menu)
     seleccion = str(input(f"Ingresa el numero del {seleccion_menu}\n"))
     listar_pokemon(seleccion, seleccion_menu, opcion)
 if opcion == "5":
     seleccion_menu = "type"
-    listar_habitat(seleccion_menu)
+    listar(seleccion_menu)
     seleccion = str(input(f"Ingresa el numero del {seleccion_menu}\n"))
     listar_pokemon(seleccion, seleccion_menu,opcion)
